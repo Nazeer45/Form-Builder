b1f95ca211d1a67a645e95ed17d0e38f192d79d5
Nazeer <nazeerazam45@gmail.com>
2025-08-09 16:23:56 +0530

    updated main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,3 @@
-// // main.tsx
-// import ReactDOM from "react-dom/client";
-// import App from "./App.tsx";
-// import { BrowserRouter } from "react-router-dom";
-// import { Provider } from "react-redux";
-// import { store } from "./store/index"; // adjust path if your store lives elsewhere
-
-// ReactDOM.createRoot(document.getElementById("root")!).render(
-//   <Provider store={store}>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </Provider>
-// );
-
-
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
